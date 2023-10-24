217ce1c12145290c6ba3e3716c09524391984a63
Your Name <you@example.com>
2023-10-24 21:12:30 +0700

    update code

--- Exercices/ss2_array_and_loop/PrintGeometry.java
+++ Exercices/ss2_array_and_loop/PrintGeometry.java
@@ -6,8 +6,8 @@ public class PrintGeometry {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         boolean flag = true;
-        int height = 0;
-        int width = 0;
+        int height=0;
+        int width=0;
         do {
             System.out.println("Menu:\n" +
                     "1: Print the rectangle\n" +
@@ -32,11 +32,52 @@ public class PrintGeometry {
                 case 2:
                     System.out.print("Input Height:");
                     height = Integer.parseInt(scanner.nextLine());
-                    for (int i = 0; i < height; i++) {
-                        for (int j = 0; j <= i; j++) {
-                            System.out.print("*");
-                        }
-                        System.out.println();
+                    System.out.println("Choose corner: ");
+                    System.out.println("1: top-left");
+                    System.out.println("2: top-right");
+                    System.out.println("3: botton-left");
+                    System.out.println("4: botton-right");
+                    int corner = Integer.parseInt(scanner.nextLine());
+                    switch (corner) {
+                        case 1:
+                            for (int i = height; i >= 1; i--) {
+                                for (int j = 1; j <= i; j++) {
+                                    System.out.print("*");
+                                }
+                                System.out.println();
+                            }
+                            break;
+                        case 2:
+                            for (int i = height; i >= 1; i--) {
+                                for (int j = 1; j <= height - i; j++) {
+                                    System.out.print(" ");
+                                }
+                                for (int j = 1; j <= i; j++) {
+
+                                    System.out.print("*");
+                                }
+                                System.out.println();
+                            }
+                            break;
+                        case 3:
+                            for (int i = 1; i <= height; i++) {
+                                for (int j = 1; j <= i; j++) {
+                                    System.out.print("*");
+                                }
+                                System.out.println();
+                            }
+                            break;
+                        case 4:
+                            for (int i = 1; i <= height; i++) {
+                                for (int j = 1; j <= height - i; j++) {
+                                    System.out.print(" ");
+                                }
+                                for (int j = 1; j <= i; j++) {
+                                    System.out.print("*");
+                                }
+                                System.out.println();
+                            }
+                            break;
                     }
                     break;
                 case 3:
