ab5ec4c8b7addd52e39b04518e64edacce664dcd
Arthur <nguyenanhtuan2111996@gmail.com>
2023-10-25 21:24:35 +0700

    upload  ss3

---
+++ Exercices/ss3_method_in_java/AddIndex.java
@@ -0,0 +1,40 @@
+package Exercices.ss3_method_in_java;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class AddIndex {
+    static Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+        System.out.println("Mời bạn nhập số lượng phần tử của mảng:");
+        int arrLength =  Integer.parseInt(scanner.nextLine());
+        int i=0;
+        int[] Arr = new int[arrLength];
+        while (i<arrLength){
+            System.out.println("Mời bạn nhập phần tử tại vị trí i-"+i);
+            Arr[i]= Integer.parseInt(scanner.nextLine());
+            i++;
+        }
+        System.out.println("Mảng vừa nhập là : " + Arrays.toString(Arr));
+        System.out.println("Nhập vị trí bạn muốn thêm: ");
+        int indexAdd = Integer.parseInt(scanner.nextLine());
+        if(indexAdd<0||indexAdd>arrLength-1){
+            System.out.println("Vị trí muốn thêm nằm ngoài  chiều dài mãng");
+        } else {
+            System.out.println("Nhập giá trị bạn muốn thêm");
+            int  valueAdd = Integer.parseInt(scanner.nextLine());
+            System.out.println(Arrays.toString(add(Arr,indexAdd,valueAdd)));}
+    }
+    public static int[] add(int[] arr,int index, int value){
+        int[] output = new int[arr.length+1];
+        for(int i = 0;i<index;i++){
+            output[i]=arr[i];
+        }
+        output[index]=value;
+        for (int i=index; i<output.length-1;i++){
+            output[i+1]=arr[i];
+        }
+        return output;
+    }
+
+}
---
+++ Exercices/ss3_method_in_java/ConcatArray.java
@@ -0,0 +1,41 @@
+package Exercices.ss3_method_in_java;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class ConcatArray {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Mời bạn nhập số lượng phần tử của mảng 1:");
+        int arr1Length =  Integer.parseInt(scanner.nextLine());
+        int i=0;
+        int[] Arr1 = new int[arr1Length];
+        while (i<arr1Length){
+            System.out.println("Mời bạn nhập phần tử tại vị trí i-"+i);
+            Arr1[i]= Integer.parseInt(scanner.nextLine());
+            i++;
+        }
+        System.out.println("Mảng vừa nhập là : " + Arrays.toString(Arr1));
+        System.out.println("Mời bạn nhập số lượng phần tử của mảng 2:");
+        int arr2Length =  Integer.parseInt(scanner.nextLine());
+        int[] Arr2 = new int[arr2Length];
+        i=0;
+        while (i<arr2Length){
+            System.out.println("Mời bạn nhập phần tử tại vị trí i-"+i);
+            Arr2[i]= Integer.parseInt(scanner.nextLine());
+            i++;
+        }
+        System.out.println("Mảng vừa nhập là : " + Arrays.toString(Arr2));
+        System.out.println("Mảng sau khi được gộp: " + Arrays.toString(concat(Arr1,Arr2)));
+    }
+    public static int[] concat(int[] arr1, int[] arr2){
+        int[] output= new int[arr2.length+ arr1.length];
+        for( int i = 0; i< arr1.length;i++){
+            output[i]=arr1[i];
+        }
+        for(int i=0;i<arr2.length;i++){
+            output[arr1.length+i]=arr2[i];
+        }
+        return output;
+    }
+}
