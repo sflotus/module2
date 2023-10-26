9c4a1735f4a540e1c8d8749a91dafe46a1ad3490
Your Name <you@example.com>
2023-10-26 20:33:47 +0700

    update SC

--- Exercices/ss3_method_in_java/FindMin.java
+++ Exercices/ss3_method_in_java/FindMin.java
@@ -1,5 +1,6 @@
 package Exercices.ss3_method_in_java;
 
+import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -7,31 +8,19 @@ public class FindMin {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         Random random = new Random();
-        System.out.println("Mời bạn nhập số hàng n:");
+        System.out.println("Mời bạn chiều dài mãng:");
         int n = Integer.parseInt((scanner.nextLine()));
-        System.out.println(" Mời bạn nhập số cột m:");
-        int m = Integer.parseInt(scanner.nextLine());
-        int[][] Arr = new int[n][m];
+        int[] Arr = new int[n];
         for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                Arr[i][j] = random.nextInt(100);
-            }
+                Arr[i] = random.nextInt(100);
         }
-        for (int i = 0; i < Arr.length; i++) {
-            for (int j = 0; j < Arr[i].length; j++) {
-                System.out.print(Arr[i][j] + " ");
-            }
-            System.out.println();
-        }
-        System.out.println("Giá trị nhỏ nhất trong mảng 2 chiều là : " + finMin(Arr));
+        System.out.println("Mảng bạn vừa nhập:"+ Arrays.toString(Arr));
+        System.out.println("Giá trị nhỏ nhất trong mảng 2 chiều là : " + findMin(Arr));
     }
-
-    public static int finMin(int[][] Arr) {
-        int minValue = Arr[0][0];
+    public static int findMin(int[] Arr) {
+        int minValue = Arr[0];
         for (int i = 0; i < Arr.length; i++) {
-            for (int j = 0; j < Arr[i].length; j++) {
-                minValue = minValue > Arr[i][j] ? Arr[i][j] : minValue;
-            }
+                minValue = minValue > Arr[i] ? Arr[i] : minValue;
         }
         return minValue;
     }
