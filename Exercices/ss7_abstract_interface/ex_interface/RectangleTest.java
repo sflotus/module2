39a36551cae01b4872cddf086ded323e5c19406e
Arthur <nguyenanhtuan2111996@gmail.com>
2023-11-06 21:01:36 +0700

    upload ss7_interface

---
+++ Exercices/ss7_abstract_interface/ex_interface/RectangleTest.java
@@ -0,0 +1,20 @@
+package Exercices.ss7_abstract_interface.ex_interface;
+
+import java.util.Random;
+
+public class RectangleTest {
+    public static void main(String[] args) {
+        Random random = new Random();
+        Rectangle rectangle = new Rectangle();
+        System.out.println(rectangle);
+        rectangle = new Rectangle(2.3, 5.8);
+        System.out.println(rectangle);
+        rectangle = new Rectangle(2.5, 3.8, "orange", true);
+        System.out.println(rectangle);
+        double percent = random.nextDouble()*100;
+        System.out.println("Add size with " + percent +"%");
+        rectangle.resize(percent);
+        System.out.println(rectangle);
+
+    }
+}
---
+++ Exercices/ss7_abstract_interface/ex_interface/Resizeable.java
@@ -0,0 +1,5 @@
+package Exercices.ss7_abstract_interface.ex_interface;
+
+public interface Resizeable {
+    void resize(double percent);
+}
