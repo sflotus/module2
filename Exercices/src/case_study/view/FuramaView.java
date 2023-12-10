468b7c651a90666db95949ac604675e021c79999
Arthur <nguyenanhtuan2111996@gmail.com>
2023-12-10 11:17:23 +0700

    update

--- Exercices/src/case_study/view/FuramaView.java
+++ Exercices/src/case_study/view/FuramaView.java
@@ -5,6 +5,7 @@ import case_study.controller.FuramaController;
 public class FuramaView {
     public static void main(String[] args) {
         FuramaController furamaController = new FuramaController();
+
         furamaController.displayMainMenu();
     }
 }
