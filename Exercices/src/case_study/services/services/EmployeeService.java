0e0d6d4c8b1f56fc357639ece196aa56489a529a
Arthur <nguyenanhtuan2111996@gmail.com>
2023-12-11 19:58:26 +0700

    update

--- Exercices/src/case_study/services/services/EmployeeService.java
+++ Exercices/src/case_study/services/services/EmployeeService.java
@@ -15,7 +15,7 @@ import java.util.regex.Pattern;
 public class EmployeeService implements IEmployeeService {
     private final static String ID_EMPLOYEE_REGEX = "[N][V][-]*(\\d{4})";
     private final static String NAME_EMPLOYEE_REGEX = "\\b[A-Z][a-z]*(?:\\s+[A-Z][a-z]*)*\\b";
-    private final static String PHONE_NUMBER_REGEX = "(|0[3|5|7|8|9])+([0-9]{10})";
+    private final static String PHONE_NUMBER_REGEX = "([0])+([0-9]{9})";
     private final static String CMND_REGEX = "^\\d{9}(?:\\d{3})?$";
     private final static String DATE_OF_BIRTH_EREGEX = "^\\d{4}\\-(0?[1-9]|1[012])\\-(0?[1-9]|[12][0-9]|3[01])$";
     private final static String EMAIL_REGEX = "^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$";
